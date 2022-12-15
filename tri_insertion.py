f9e36e2ac407b3c92a191f7e7edefc02041ca23c
Lucastor05 <lucasdeniel05@gmail.com>
2022-12-15 09:41:37 +0100

    add comparaison affectation dans tri insertion

--- tri_insertion.py
+++ tri_insertion.py
@@ -1,16 +1,37 @@
-tableau = [15,20,12,3,11,5,2]
-a = (len(tableau))
-print(a)
+import random
+
+tableau = []
+
+for i in range(10):
+    tableau.append(random.randint(0,200))
+
 def tri_insertion (tableau):
+    comparaison = 0
+    affectation = 0
+    l = []
+
     for i in range(1,len(tableau)):
 
         lacase = tableau[i]
+        affectation+=1
         j = i
+        affectation+=1
+
         while j>0 and tableau[j-1]>lacase:
+            comparaison+=2
+
             tableau[j]=tableau[j-1]
+            affectation += 1
+
             j=j-1
+            affectation +=1
+
         tableau[j]= lacase
-        print(tableau)
+        affectation+=1
 
+    l.append(tableau)
+    l.append(comparaison)
+    l.append(affectation)
+    return l
 
-tri_insertion(tableau)
+print(tri_insertion(tableau))
