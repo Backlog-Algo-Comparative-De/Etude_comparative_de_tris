215b7de033a9148da20b58c20cf266b966ff6eea
mounir <mounirxchitour@gmail.com>
2022-12-15 21:50:58 +0100

    compteur Step

--- tri_insertion.py
+++ tri_insertion.py
@@ -1,3 +1,8 @@
+tableau = [15,20,12,3,11,5,2,8,14]
+pireDesCas = [10,9,8,7,6,5,4,3,2,1]
+meilleurDesCas=[1,2,3,4,5,6,7,8,9,10],
+a = (len(tableau))
+print(a)
 def tri_insertion (tableau):
     comparaison = 0
     affectation = 0
@@ -21,7 +26,8 @@ def tri_insertion (tableau):
             affectation +=1
 
         tableau[j]= lacase
-        affectation+=1
+        print(tableau,i)
+
 
     l.append(tableau)
     l.append(comparaison + affectation)
