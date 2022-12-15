9e4c65f91ea65c6eaada76a2753da4affe8c65bb
mounir <mounirxchitour@gmail.com>
2022-12-15 21:50:58 +0100

    compteur Step

--- tri_insertion.py
+++ tri_insertion.py
@@ -1,4 +1,6 @@
-tableau = [15,20,12,3,11,5,2]
+tableau = [15,20,12,3,11,5,2,8,14]
+pireDesCas = [10,9,8,7,6,5,4,3,2,1]
+meilleurDesCas=[1,2,3,4,5,6,7,8,9,10],
 a = (len(tableau))
 print(a)
 def tri_insertion (tableau):
@@ -10,7 +12,7 @@ def tri_insertion (tableau):
             tableau[j]=tableau[j-1]
             j=j-1
         tableau[j]= lacase
-        print(tableau)
+        print(tableau,i)
 
 
 tri_insertion(tableau)
