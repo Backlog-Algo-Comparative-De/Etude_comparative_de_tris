32a6a538259aa7fb0b007b4639bd5915610074a1
BastienVitour <bvitour2004@gmail.com>
2022-12-16 08:57:03 +0100

    amélioré main

--- main.py
+++ main.py
@@ -13,26 +13,37 @@ def stat(min, max, step, nbr):
 
     for i in range(min, max + 1, step):
 
+        moyenne = 0
+
         for j in range(nbr):
+
+            moyenne = 0
 
             tableau = []
 
             for k in range(i):
 
-                tableau.append(randint(1, 10))
+                tableau.append(randint(1, 100))
 
             if tri == 1:
                 moyenne = moyenne + tri_selection(tableau)[1]
+                # print(moyenne)
+                # print(tri_selection(tableau))
             if tri == 2:
                 moyenne = moyenne + tri_insertion(tableau)[1]
             if tri == 3:
                 moyenne = moyenne + tri_a_bulle(tableau)[1]
+                # print(moyenne)
             if tri == 4:
                 moyenne = moyenne + tri_bulle_opti(tableau)[1]
 
-        moyenne = moyenne / i
+        # print(i)
+        # print(moyenne)
+        moyenne = moyenne / nbr
 
         print(f"Taille du tableau : {i}, nombre moyen d'opérations : {moyenne}")
+
+    stat(10, 20, 5, 10)
 
 
 stat(10, 20, 5, 10)
